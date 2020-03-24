6cf64679de4b35b62ce57fb42dde808167aa4525
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-03-24 19:13:21 +0900

    update jupyterlab_templates config

--- jupyterlab/jupyter_notebook_config.py
+++ jupyterlab/jupyter_notebook_config.py
@@ -1,2 +1,6 @@
 # 他は setting.py の NOTEBOOK_ARGUMENTS で 実行時に渡す
 c.NotebookApp.token = ''
+
+c.JupyterLabTemplates.template_dirs = ['list', 'of', 'template', 'directories']
+c.JupyterLabTemplates.include_default = True
+c.JupyterLabTemplates.include_core_paths = True
